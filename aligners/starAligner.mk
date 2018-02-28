8f550b6c289f519d7e987de861785db3f4ceadfc
charlottekyng <ngkiuy@login10.cluster.bc2.ch>
2018-02-28 23:15:03 +0100

    added alignment stats from STAR

--- aligners/starAligner.mk
+++ aligners/starAligner.mk
@@ -16,7 +16,7 @@ else
 STAR_BAMS = $(foreach sample,$(SAMPLES),star/secondpass/$(sample).star.bam)
 endif
 
-star : $(STAR_BAMS) $(addsuffix .bai,$(STAR_BAMS)) star/all.ReadsPerGene.out.tab
+star : $(STAR_BAMS) $(addsuffix .bai,$(STAR_BAMS)) star/all$(PROJECT_PREFIX).ReadsPerGene.out.tab star/all$(PROJECT_PREFIX).alignment_stats.txt 
 
 star/firstpass/%.SJ.out.tab : fastq/%.1.fastq.gz $(if $(findstring true,$(PAIRED_END)),fastq/%.2.fastq.gz)
 	$(call RUN,4,$(RESOURCE_REQ_MEDIUM_MEM),$(RESOURCE_REQ_SHORT),$(STAR_MODULE),"\
@@ -63,36 +63,23 @@ bam/%.bam : star/secondpass/%.star.$(BAM_SUFFIX)
 	$(INIT) ln -f $< $@
 
 
-#define align-star-secondpass
-#star/secondpass/$1.star.bam : fastq/$1.1.fastq.gz $(if $(findstring true,$(PAIRED_END)),fastq/$1.2.fastq.gz) $(foreach sample,$(SAMPLES),star/firstpass/$(sample).SJ.out.tab)
-#	$$(call LSCRIPT_PARALLEL_MEM,8,$$(RESOURCE_REQ_HIGHMEM),$$(RESOURCE_REQ_LONG),"$$(MKDIR) star star/secondpass/; \
-#	$$(LOAD_STAR_MODULE); STAR --runMode alignReads \
-#	--runThreadN 8 --genomeDir $$(STAR_GENOME_DIR) --readFilesIn $$< $$(if $$(findstring true,$(PAIRED_END)),$$(word 2,$$^)) \
-#	--readFilesCommand gunzip -c \
-#	--alignSJoverhangMin 8 --alignSJDBoverhangMin 10 --alignIntronMax 200000 --alignMatesGapMax 200000 \
-#	--alignSJstitchMismatchNmax 5 -1 5 5 --limitSjdbInsertNsj 5000000 \
-#	--outFilterType BySJout --outFilterMultimapNmax 20 --outFilterMismatchNmax 999 \
-#	--outFileNamePrefix star/secondpass/$$(basename $$(basename $$(notdir $$@))). \
-#	--sjdbFileChrStartEnd $$(filter %.SJ.out.tab,$$^) \
-#	--outSAMprimaryFlag AllBestScore --outSAMtype BAM SortedByCoordinate \
-#	--outReadsUnmapped None --outMultimapperOrder Random --outSAMattrIHstart 0 \
-#	--chimSegmentMin 12 --chimJunctionOverhangMin 12 --chimSegmentReadGapMax parameter 3 \
-#	--quantMode GeneCounts TranscriptomeSAM && mv star/secondpass/$1.Aligned.sortedByCoord.out.bam star/secondpass/$1.star.bam")
-
-#star/secondpass/$1.Chimeric.out.junction : star/secondpass/$1.star.bam
-#star/secondpass/$1.ReadsPerGene.out.tab : star/secondpass/$1.star.bam
-#star/secondpass/$1.Aligned.toTranscriptome.out.bam : star/secondpass/$1.star.bam
-#endef
-#$(foreach sample,$(SAMPLES),\
-#	$(eval $(call align-star-secondpass,$(sample))))
-
-#bam/%.bam : star/secondpass/%.star.bam
-#	$(INIT) ln -f $< $@
-
-star/all.ReadsPerGene.out.tab : $(foreach sample,$(SAMPLES),star/secondpass/$(sample).ReadsPerGene.out.tab)
+star/all$(PROJECT_PREFIX).ReadsPerGene.out.tab : $(foreach sample,$(SAMPLES),star/secondpass/$(sample).ReadsPerGene.out.tab)
 	perl -p -e "s/N_unmapped/GENE\t\t\t\nN_unmapped/;" `ls star/secondpass/*.ReadsPerGene.out.tab|head -1` | cut -f 1 > $@; \
 	for x in $^; do sample=`echo $$x | sed 's/.*\///; s/\..*//'`; perl -p -e "s/N_unmapped/\t$$sample\t\t\nN_unmapped/;" \
 	star/secondpass/$$sample.ReadsPerGene.out.tab | cut -f 2 | paste $@ - > $@.tmp; mv $@.tmp $@; done
+
+star/all$(PROJECT_PREFIX).alignment_stats.txt : $(foreach sample,$(SAMPLES),star/secondpass/$(sample).Log.final.out)
+	$(INIT) \
+	{ \
+	grep "|" $< | cut -f1 -d '|' | perl -p -e "s/ //g; s/\n/\t/g;"; echo ""; \
+	for metrics in $^; do \
+		samplename=$$(basename $${metrics%%.Log.final.out}); \
+		echo -n $$samplename;\
+		grep "|" $$metrics | cut -f2 -d '|' | perl -p -e "s/ //g; s/\n/\t/g;"; echo ""; \
+	done; \
+	} > $@
+
+
 
 include usb-modules-v2/fastq_tools/fastq.mk
 include usb-modules-v2/bam_tools/processBam.mk
