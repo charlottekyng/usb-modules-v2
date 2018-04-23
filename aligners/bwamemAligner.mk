eff419036b11bf72d2cc81d9341dcf7964cf595a
charlottekyng <ngkiuy@login11.cluster.bc2.ch>
2018-04-23 12:38:38 +0200

    bwa aln does not use the same paramters as bwamem (-M is not valid in bwaaln)

--- aligners/bwamemAligner.mk
+++ aligners/bwamemAligner.mk
@@ -12,7 +12,7 @@ LOGDIR ?= log/bwamem.$(NOW)
 
 VPATH ?= unprocessed_bam
 
-..DUMMY := $(shell mkdir -p version; $(BWA) &> version/bwamem.txt; echo "options: $(BWA_ALN_OPTS)" >> version/bwamem.txt )
+..DUMMY := $(shell mkdir -p version; $(BWA) &> version/bwamem.txt; echo "options: $(BWA_MEM_OPTS)" >> version/bwamem.txt )
 .SECONDARY:
 .DELETE_ON_ERROR: 
 .PHONY: bwamem
@@ -32,7 +32,7 @@ ifeq ($(MERGE_SPLIT_FASTQ),true)
 bwamem/bam/%.bwamem.bam : fastq/%.1.fastq.gz $(if $(findstring true,$(PAIRED_END)),fastq/%.2.fastq.gz)
 	LBID=`echo "$*" | sed 's/_[A-Za-z0-9]\+//'`; \
 	$(call RUN,$(BWA_NUM_CORES),$(RESOURCE_REQ_LOW_MEM),$(RESOURCE_REQ_MEDIUM),$(BWA_MODULE) $(SAMTOOLS_MODULE),"\
-	$(BWA_MEM) -t $(BWA_NUM_CORES) $(BWA_ALN_OPTS) \
+	$(BWA_MEM) -t $(BWA_NUM_CORES) $(BWA_MEM_OPTS) \
 	-R \"@RG\tID:$*\tLB:$${LBID}\tPL:${SEQ_PLATFORM}\tSM:$${LBID}\" $(REF_FASTA) $^ | \
 	$(SAMTOOLS) view -bhS - > $@")
 else
@@ -40,7 +40,7 @@ define align-split-fastq
 bwamem/bam/$1.bwamem.bam : fastq/$1.1.fastq.gz $$(if $$(findstring true,$$(PAIRED_END)),fastq/$1.2.fastq.gz)
 	LBID=`echo "$1" | sed 's/_[A-Za-z0-9]\+//'`; \
 	$$(call RUN,$$(BWA_NUM_CORES),$$(RESOURCE_REQ_LOW_MEM),$$(RESOURCE_REQ_MEDIUM),$$(BWA_MODULE) $$(SAMTOOLS_MODULE),"\
-	$$(BWA_MEM) -t $$(BWA_NUM_CORES) $$(BWA_ALN_OPTS) \
+	$$(BWA_MEM) -t $$(BWA_NUM_CORES) $$(BWA_MEM_OPTS) \
 	-R \"@RG\tID:$1\tLB:$$$${LBID}\tPL:$${SEQ_PLATFORM}\tSM:$$$${LBID}\" $$(REF_FASTA) $$^ | \
 	$$(SAMTOOLS) view -bhS - > $$@")
 endef
