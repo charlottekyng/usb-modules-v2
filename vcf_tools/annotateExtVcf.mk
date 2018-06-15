03e21f668b93aa67a2f3904b9775df5c98e1d981
charlottekyng <ngkiuy@login11.cluster.bc2.ch>
2018-06-15 15:41:04 +0200

    ann_ext_vcf now seems to work

--- vcf_tools/annotateExtVcf.mk
+++ vcf_tools/annotateExtVcf.mk
@@ -1,19 +1,20 @@
 include usb-modules-v2/Makefile.inc
 
 ifeq ($(findstring true,$(SOMATIC_ANN)),true)
+USE_SUFAM = false
 include usb-modules-v2/variant_callers/somatic/somaticVariantCaller.inc
 else
 include usb-modules-v2/variant_callers/variantCaller.inc
 endif
 
+EXT_NAMES ?=
+SOMATIC_FILTERS :=
+
+
 LOGDIR ?= log/ann_ext_vcf.$(NOW)
 
-EXT_NAMES ?=
-
-SOMATIC_FILTERS := 
-
-
 ann_ext_vcf : ext_vcfs ext_tables
+
 
 ifeq ($(findstring true,$(SOMATIC_ANN)),true)
 ext_vcfs : $(foreach ext_name,$(EXT_NAMES),$(call SOMATIC_VCFS,$(ext_name)))
@@ -23,7 +24,6 @@ ext_vcfs : $(foreach ext_name,$(EXT_NAMES),$(call VCFS,$(EXT_NAME)))
 ext_tables : $(foreach ext_name,$(EXT_NAMES),$(call TABLES,$(EXT_NAME)))
 endif
 
-$(info $(EXT_NAMES))
 
 .DELETE_ON_ERROR:
 .SECONDARY:
