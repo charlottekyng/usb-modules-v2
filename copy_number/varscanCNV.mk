0385cc8522945aa865a13f52c0b1a888c46c89fe
charlottekyng <ngkiuy@login11.cluster.bc2.ch>
2018-08-02 17:11:48 +0200

    varscan_cnv now takes additiona parameters re: regions/probes to throw out based on depth in normal no defaults - but for panels not tested with large pool of normals (to look at variation), this is useful for getting rid of probes/amplicons of low depth

--- copy_number/varscanCNV.mk
+++ copy_number/varscanCNV.mk
@@ -74,6 +74,7 @@ varscan/segment/$1_$2.segment.Rdata : $$(foreach pool,$$(TARGETS_FILE_INTERVALS_
 	$$(call RUN,1,$$(RESOURCE_REQ_LOW_MEM),$$(RESOURCE_REQ_VSHORT),$$(R_MODULE),"\
 	$$(CBS_SEGMENTCNV) --alpha $$(CBS_SEG_ALPHA) --smoothRegion $$(CBS_SEG_SMOOTH) \
 	--trim $$(CBS_TRIM) --clen $$(CBS_CLEN) --undoSD $$(CBS_SEG_SD) --separate_arm_seg TRUE \
+	--excl_N_outlier_pc $$(CBS_EXCL_N_OUTLIER_PC) --minNdepth $$(CBS_MIN_N_DEPTH) \
 	$$(if $$(CENTROMERE_TABLE),--centromereFile=$$(CENTROMERE_TABLE)) --prefix=$$(@D)/$1_$2 $$^")
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
