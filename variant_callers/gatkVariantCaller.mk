79e606a0d81d6ca2e7dc9c45f0d6f481122a4e6c
charlottekyng <ngkiuy@login10.cluster.bc2.ch>
2018-10-30 11:28:14 +0100

    mutect o ther ops, various vcf extentions

--- variant_callers/gatkVariantCaller.mk
+++ variant_callers/gatkVariantCaller.mk
@@ -14,7 +14,8 @@ PHONY += gatk
 
 
 #gatk : gatk/gatk_indels.filtered.vcf.gz gatk/gatk_snps.filtered.vcf.gz
-gatk : $(foreach type,$(VARIANT_TYPES),vcf/all$(PROJECT_PREFIX).$(type).$(VCF_ANNS_SUFFIX).vcf)
+gatk : $(foreach type,$(VARIANT_TYPES),vcf/all$(PROJECT_PREFIX).$(type).$(VCF_ANNS_SUFFIX).vcf) \
+$(foreach type,$(VARIANT_TYPES),tables/all$(PROJECT_PREFIX).$(type).$(VCF_ANNS_SUFFIX).tab.txt)
 # vcf/all$(PROJECT_PREFIX).gatk_indels.$(VCF_ANNS_SUFFIX).vcf.gz vcf/all$(PROJECT_PREFIX).gatk_snps.$(VCF_ANNS_SUFFIX).vcf.gz
 #gatk : gatk_vcfs $(if $(findstring NONE,$(PANEL)),gatk_vcf_stats,gatk_tables)
 #gatk_vcfs : $(foreach type,$(VARIANT_TYPES),$(call MAKE_VCF_FILE_LIST,$(type)) $(addsuffix .idx,$(call MAKE_VCF_FILE_LIST,$(type))))
