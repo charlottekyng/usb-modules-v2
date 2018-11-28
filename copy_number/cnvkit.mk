63d0a38e3f3ab4447a6bccbf784a9cd367b79336
charlottekyng <ngkiuy@login10.cluster.bc2.ch>
2018-11-28 23:18:18 +0100

    for cnvkit, add median centering before segmentation, add optional scaling of segments for multi-sample cases

--- copy_number/cnvkit.mk
+++ copy_number/cnvkit.mk
@@ -7,7 +7,8 @@ LOGDIR ?= log/cnvkit.$(NOW)
 .PHONY : cnvkit
 
 cnvkit : cnvkit/cns/all$(PROJECT_PREFIX).heatmap.pdf cnvkit/cns/all$(PROJECT_PREFIX).geneCN.GL_LRR.pdf \
-cnvkit/cns/all$(PROJECT_PREFIX).geneCN.log2.pdf cnvkit/cns/all$(PROJECT_PREFIX).cns.pdf.tar.gz
+cnvkit/cns/all$(PROJECT_PREFIX).geneCN.log2.pdf cnvkit/cns/all$(PROJECT_PREFIX).cns.pdf.tar.gz \
+cnvkit/cns/all$(PROJECT_PREFIX).heatmap.scaled.pdf cnvkit/cns/all$(PROJECT_PREFIX).geneCN.scaled.log2.pdf
 
 cnvkit/rsem/%.genes.results : rsem/%.genes.results
 	$(MKDIR) cnvkit cnvkit/rsem; \
@@ -29,21 +30,50 @@ cnvkit/all$(PROJECT_PREFIX).summary.out : $(foreach sample,$(SAMPLES),cnvkit/rse
 
 cnvkit/cnr/%.cnr : cnvkit/all$(PROJECT_PREFIX).summary.out
 	
-	
-cnvkit/cns/%.cns : cnvkit/cnr/%.cnr
+
+cnvkit/cnr/%.cnr.median : cnvkit/cnr/%.cnr
+	$(call RUN,1,$(RESOURCE_REQ_LOW_MEM),$(RESOURCE_REQ_SHORT),$(R_MODULE),"\
+	$(CNVKIT_MEDIAN_CENTER) --inputFile $< --outputFile $@")
+
+cnvkit/cns/%.cns : cnvkit/cnr/%.cnr.median
 	$(call RUN,1,$(RESOURCE_REQ_LOW_MEM),$(RESOURCE_REQ_SHORT),$(SINGULARITY_MODULE),"\
 	$(SINGULARITY_EXEC) $(CNVKIT_IMG) $(CNVKIT) segment \
 	./$< -o ./$@ --drop-low-coverage")
 	
-cnvkit/cns/%.cns.pdf : cnvkit/cns/%.cns cnvkit/cnr/%.cnr
+cnvkit/cns/%.cns.pdf : cnvkit/cns/%.cns cnvkit/cnr/%.cnr.median
 	$(call RUN,1,$(RESOURCE_REQ_LOW_MEM),$(RESOURCE_REQ_SHORT),$(SINGULARITY_MODULE),"\
 	$(SINGULARITY_EXEC) $(CNVKIT_IMG) $(CNVKIT) scatter \
 	-s ./$< -o ./$@ $(<<)")
+
+define cnvkit-scale-segments
+cnvkit/cns/$1.cns.scaled : $$(foreach sample,$2,cnvkit/cns/$$(sample).cns) cnvkit/cns/$1.cns
+	$$(call RUN,1,$$(RESOURCE_REQ_LOW_MEM),$$(RESOURCE_REQ_VSHORT),$$(R_MODULE),"\
+	$$(CNVKIT_SCALE_SEGMENTS) $$^")
+endef
+$(foreach set,$(SAMPLE_SETS),\
+	$(eval $(call cnvkit-scale-segments,$(lastword $(subst _,$( ),$(set))),\
+	$(wordlist 1,$(shell expr $(words $(subst _,$( ),$(set))) - 1),$(subst _,$( ),$(set))))))
+
+define cnvkit-scale-segments2
+cnvkit/cns/$1.cns.scaled : cnvkit/cns/$2.cns.scaled
 	
+endef
+$(foreach set,$(SAMPLE_SETS),\
+	$(foreach sample,$(wordlist 1,$(shell expr $(words $(subst _,$( ),$(set))) - 1),$(subst _,$( ),$(set))),\
+		$(eval $(call cnvkit-scale-segments2,$(sample),$(lastword $(subst _,$( ),$(set)))))))
+
+cnvkit/cnr/%.cnr.scaled : cnvkit/cnr/%.cnr
+	ln $< $@
+
 cnvkit/cns/all$(PROJECT_PREFIX).heatmap.pdf : $(foreach sample,$(SAMPLES),cnvkit/cns/$(sample).cns)
 	$(call RUN,1,$(RESOURCE_REQ_LOW_MEM),$(RESOURCE_REQ_SHORT),$(SINGULARITY_MODULE),"\
 	$(SINGULARITY_EXEC) $(CNVKIT_IMG) $(CNVKIT) heatmap -d \
 	$(foreach sample,$(SAMPLES),./cnvkit/cns/$(sample).cns) -o ./$@")
+
+cnvkit/cns/all$(PROJECT_PREFIX).heatmap.scaled.pdf : $(foreach sample,$(SAMPLES),cnvkit/cns/$(sample).cns.scaled)
+	$(call RUN,1,$(RESOURCE_REQ_LOW_MEM),$(RESOURCE_REQ_SHORT),$(SINGULARITY_MODULE),"\
+	$(SINGULARITY_EXEC) $(CNVKIT_IMG) $(CNVKIT) heatmap -d \
+	$(foreach sample,$(SAMPLES),./cnvkit/cns/$(sample).cns.scaled) -o ./$@")
 	
 cnvkit/cns/all$(PROJECT_PREFIX).cns.pdf.tar.gz : $(foreach sample,$(SAMPLES),cnvkit/cns/$(sample).cns.pdf)
 	$(INIT) tar -czf $@ $^
@@ -59,8 +89,10 @@ cnvkit/cns/all$(PROJECT_PREFIX).geneCN.GL_LRR.txt : $(foreach sample,$(SAMPLES),
 
 cnvkit/cns/all$(PROJECT_PREFIX).geneCN.log2.txt : cnvkit/cns/all$(PROJECT_PREFIX).geneCN.GL_LRR.txt
 	
-	
 
-
+cnvkit/cns/all$(PROJECT_PREFIX).geneCN.scaled.log2.txt : $(foreach sample,$(SAMPLES),cnvkit/cns/$(sample).cns.scaled) $(foreach sample,$(SAMPLES),cnvkit/cnr/$(sample).cnr.scaled)
+	$(call RUN,1,$(RESOURCE_REQ_HIGH_MEM),$(RESOURCE_REQ_SHORT),$(R_MODULE),"\
+	$(CNVKIT_GENE_CN) $(CNVKIT_GENE_CN_OPTS) --summaryType log2 \
+	--outFile $(@D)/all$(PROJECT_PREFIX).geneCN.scaled $(filter %.cns.scaled,$^)")
 
 #include usb-modules-v2/rnaseq/rsem.mk
--- copy_number/cnvkit_process_rsem_input.R
+++ copy_number/cnvkit_process_rsem_input.R
@@ -7,7 +7,8 @@ optList <- list(
 	make_option('--inputRSEMFile', action='store', default = 'all.genes.expected_count.results', help = 'input RSEM file to be normalized'),
 	make_option('--gtf', action='store', default = NULL, help = 'GTF annotation file if gene subset is required'),
 	make_option('--geneBiotype', action='store', default = NULL, help = 'gene biotype/s to include'),
-	make_option('--outputFile', action='store', default = NULL, help = 'output file'))
+	make_option('--outputFile', action='store', default = NULL, help = 'output file'),
+	make_option('--chromosomes', action='store', default = 1:22, help = 'vector of chromosomes to include'))
 
 parser <- OptionParser(usage = "%prog", option_list = optList);
 opt <- parse_args(parser, positional_arguments = F);
@@ -32,6 +33,10 @@ if (!is.null(opt$gtf)){
 		} else { cat ("geneBiotype provided but no appropriate column found in GTF, using all genes in the GTF.\n") }
 
 	} else { cat("No geneBiotype provided, using all genes in the GTF.\n") }
+	if (!is.null(opt$chromosomes)) {
+		gtf <- gtf[which(seqnames(gtf) %in% opt$chromosomes),]
+	} else { cat ("No chromosomes provided, using all chromosomes in GTF.\n") }
+
 } else {
 	cat("GTF file not provided, not annotation will be done\n")
 }
