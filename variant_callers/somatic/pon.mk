b710d774d8e6db9e128a1f053c3facc166fdf289
charlottekyng <ngkiuy@login10.cluster.bc2.ch>
2019-01-03 19:20:32 +0100

    PoN construction now uses GATK4 Mutect2

--- variant_callers/somatic/pon.mk
+++ variant_callers/somatic/pon.mk
@@ -25,34 +25,50 @@ else
 
 pon : mutect2/pon.mutect2.vcf
 
+#define mutect2-pon-chr
+#mutect2/chr_vcf_pon/$1.$2.mutect2.vcf : bam/$1.bam
+#	$$(MKDIR) mutect2/chr_vcf_pon; $$(call RUN,1,$$(RESOURCE_REQ_HIGH_MEM),$$(RESOURCE_REQ_LONG),$$(JAVA8_MODULE),"\
+#	$$(call GATK,MuTect2,$$(RESOURCE_REQ_HIGH_MEM_JAVA)) \
+#	--reference_sequence $$(REF_FASTA) --input_file:tumor $$< --artifact_detection_mode \
+#	--dbsnp $$(DBSNP) $$(if $$(findstring GRCm38,$$(REF)),,--cosmic $$(COSMIC)) --intervals $2 \
+#	--annotation TandemRepeatAnnotator --annotation OxoGReadCounts \
+#	--out mutect2/chr_vcf_pon/$1.$2.mutect2.vcf")
+#endef
+#$(foreach chr,$(CHROMOSOMES), \
+#	$(foreach normal,$(PANEL_OF_NORMAL_SAMPLES), \
+#		$(eval $(call mutect2-pon-chr,$(normal),$(chr)))))
+
+#define mutect2-pon-chr-merge
+#mutect2/chr_vcf_pon/pon.$1.mutect2.vcf : $$(foreach normal,$$(PANEL_OF_NORMAL_SAMPLES),mutect2/chr_vcf_pon/$$(normal).$1.mutect2.vcf)
+#	$$(call RUN,1,$$(RESOURCE_REQ_HIGH_MEM),$$(RESOURCE_REQ_SHORT),$$(JAVA8_MODULE),"\
+#	$$(call GATK,CombineVariants,$$(RESOURCE_REQ_HIGH_MEM_JAVA)) --reference_sequence $(REF_FASTA) \
+#	-minN $$(PON_MIN_SAMPLES) --setKey \"null\" --filteredAreUncalled --filteredrecordsmergetype KEEP_IF_ANY_UNFILTERED \
+#	$$(foreach normal,$$(PANEL_OF _NORMAL_SAMPLES),--variant mutect2/chr_vcf_pon/$$(normal).$1.mutect2.vcf) \
+#	--intervals $1 --out $$@")
+#endef
+#$(foreach chr,$(CHROMOSOMES), \
+#	$(eval $(call mutect2-pon-chr-merge,$(chr))))
+
+#mutect2/pon.mutect2.vcf : $(foreach chr,$(CHROMOSOMES),mutect2/chr_vcf_pon/pon.$(chr).mutect2.vcf)
+#	$(call RUN,1,$(RESOURCE_REQ_HIGH_MEM),$(RESOURCE_REQ_SHORT),$(JAVA8_MODULE),"\
+#	$(call GATK,CombineVariants,$(RESOURCE_REQ_HIGH_MEM_JAVA)) --reference_sequence $(REF_FASTA) \
+#	$(foreach chr,$(CHROMOSOMES),--variant mutect2/chr_vcf_pon/pon.$(chr).mutect2.vcf) \
+#	--genotypemergeoption UNIQUIFY --out $@")
+
 define mutect2-pon-chr
-mutect2/chr_vcf_pon/$1.$2.mutect2.vcf : bam/$1.bam
-	$$(MKDIR) mutect2/chr_vcf_pon; $$(call RUN,1,$$(RESOURCE_REQ_HIGH_MEM),$$(RESOURCE_REQ_LONG),$$(JAVA8_MODULE),"\
-	$$(call GATK,MuTect2,$$(RESOURCE_REQ_HIGH_MEM_JAVA)) \
-	--reference_sequence $$(REF_FASTA) --input_file:tumor $$< --artifact_detection_mode \
-	--dbsnp $$(DBSNP) $$(if $$(findstring GRCm38,$$(REF)),,--cosmic $$(COSMIC)) --intervals $2 \
-	--annotation TandemRepeatAnnotator --annotation OxoGReadCounts \
-	--out mutect2/chr_vcf_pon/$1.$2.mutect2.vcf")
+mutect2/chr_vcf_pon/$1.$2.mutect2.vcf.gz : bam/$1.bam
+	$$(MKDIR) mutect2/chr_vcf_pon; \
+	$$(call RUN,1,$$(RESOURCE_REQ_HIGH_MEM),$$(RESOURCE_REQ_LONG),$$(GATK40_MODULE),"\
+	$$(call GATK40,Mutect2,$$(RESOURCE_REQ_HIGH_MEM_JAVA)) \
+	-R $$(REF_FASTA) -I $$< -tumor $1 -L $2 -O $$@ \
+	--disable-read-filter MateOnSameContigOrNoMappedMateReadFilter")
 endef
 $(foreach chr,$(CHROMOSOMES), \
 	$(foreach normal,$(PANEL_OF_NORMAL_SAMPLES), \
 		$(eval $(call mutect2-pon-chr,$(normal),$(chr)))))
 
-define mutect2-pon-chr-merge
-mutect2/chr_vcf_pon/pon.$1.mutect2.vcf : $$(foreach normal,$$(PANEL_OF_NORMAL_SAMPLES),mutect2/chr_vcf_pon/$$(normal).$1.mutect2.vcf)
-	$$(call RUN,1,$$(RESOURCE_REQ_HIGH_MEM),$$(RESOURCE_REQ_SHORT),$$(JAVA8_MODULE),"\
-	$$(call GATK,CombineVariants,$$(RESOURCE_REQ_HIGH_MEM_JAVA)) --reference_sequence $(REF_FASTA) \
-	-minN 2 --setKey \"null\" --filteredAreUncalled --filteredrecordsmergetype KEEP_IF_ANY_UNFILTERED \
-	$$(foreach normal,$$(NORMAL_SAMPLES),--variant mutect2/chr_vcf_pon/$$(normal).$1.mutect2.vcf) \
-	--intervals $1 --out $$@")
-endef
-$(foreach chr,$(CHROMOSOMES), \
-	$(eval $(call mutect2-pon-chr-merge,$(chr))))
-
-mutect2/pon.mutect2.vcf : $(foreach chr,$(CHROMOSOMES),mutect2/chr_vcf_pon/pon.$(chr).mutect2.vcf)
-	$(call RUN,1,$(RESOURCE_REQ_HIGH_MEM),$(RESOURCE_REQ_SHORT),$(JAVA8_MODULE),"\
-	$(call GATK,CombineVariants,$(RESOURCE_REQ_HIGH_MEM_JAVA)) --reference_sequence $(REF_FASTA) \
-	$(foreach chr,$(CHROMOSOMES),--variant mutect2/chr_vcf_pon/pon.$(chr).mutect2.vcf) \
-	--genotypemergeoption UNIQUIFY --out $@")
-
+mutect2/pon.mutect2.vcf : $(foreach chr,$(CHROMOSOMES),$(foreach normal,$(PANEL_OF_NORMAL_SAMPLES),mutect2/chr_vcf_pon/$(normal).$(chr).mutect2.vcf.gz))
+	$(call RUN,1,$(RESOURCE_REQ_HIGH_MEM),$(RESOURCE_REQ_LONG),$(GATK40_MODULE),"\
+	$(call GATK40,CreateSomaticPanelOfNormals,$(RESOURCE_REQ_HIGH_MEM_JAVA)) \
+	$(foreach f,$^,-vcfs $(f)) -O $@ --duplicate-sample-strategy ALLOW_ALL --min-sample-count $(PON_MIN_SAMPLES)")
 endif
