ec74002809f10c5216c0c37115e82db4e1dd16d4
abenjak <37447134+abenjak@users.noreply.github.com>
2019-09-11 10:37:10 +0200

    fixed bug in facetsGeneCN.R
    Fixed bug (line 158) where genes reported in ampdel.txt would not correspond to the right chromosome (they were always from chr1).
    Added cf.em to the summary report.

--- copy_number/facetsGeneCN.R
+++ copy_number/facetsGeneCN.R
@@ -20,7 +20,7 @@ suppressPackageStartupMessages(library("facets"));
 
 optList <- list(
 	make_option("--outFile", default = NULL, help = "output file"),
-	make_option("--summaryType", default = c("GL_ASCNA", "GL_LRR", "cnlr.median", "tcn.em", "lcn.em")),
+	make_option("--summaryType", default = c("GL_ASCNA", "GL_LRR", "cnlr.median", "tcn.em", "lcn.em", "cf.em")),
 #	make_option("--mysqlHost", default = '10.0.200.48', help = "MySQL server hostname"),
 #	make_option("--mysqlPort", default = 38493, help = "MySQL server port"),
 #	make_option("--mysqlUser", default = 'embl', help = "MySQL server username"),
@@ -44,9 +44,9 @@ if (length(arguments$args) < 1) {
         cat("Need genes files\n")
         print_help(parser);
         stop();
-} else if (!all(opt$summaryType %in% c("GL_ASCNA", "GL_LRR", "tcn.em", "lcn.em", "cnlr.median"))) {
-	cat("summaryType can only be one or more of GL_ASCNA, GL_LRR, tcn.em, lcn.em, cnlr.median\n")
-	print_help(prase);
+} else if (!all(opt$summaryType %in% c("GL_ASCNA", "GL_LRR", "tcn.em", "lcn.em", "cnlr.median", "cf.em"))) {
+	cat("summaryType can only be one or more of GL_ASCNA, GL_LRR, tcn.em, lcn.em, cnlr.median, cf.em\n")
+	print_help(parser);
 	stop();
 } else {
 	facetsFiles <- arguments$args
@@ -99,7 +99,7 @@ mm <- lapply(facetsFiles, function(f) {
 		df$GL_LRR[df$cnlr.median > median(lrr)+(2*sd(lrr))] <- 1
 		df$GL_LRR[df$cnlr.median > median(lrr)+(6*sd(lrr))] <- 2
 	}
-	df %>% select(hgnc, GL_ASCNA, GL_LRR, tcn.em, lcn.em, cnlr.median) %>% ungroup
+	df %>% select(hgnc, GL_ASCNA, GL_LRR, tcn.em, lcn.em, cnlr.median, cf.em) %>% ungroup
 })
 names(mm) <- facetsFiles
 for (f in facetsFiles) {
@@ -119,11 +119,11 @@ mm <- cbind(genes, bind_cols(mm))
 cat ("Finished bind_cols...\n")
 save.image(paste(opt$outFile, ".RData", sep=""))
 
-seg_sample <- seg_chr <- seg_band <- seg_start <- seg_end <- seg_cnlr <- seg_genes <- seg_type <- seg_GLtype <- NA
+seg_sample <- seg_chr <- seg_band <- seg_start <- seg_end <- seg_cnlr <- seg_genes <- seg_type <- seg_GLtype <- seg_cf.em <- NA
 
 for (i in grep("GL", colnames(mm))) {
 	for(chr in intersect(c(1:22,"X"), unique(mm$chrom))) {
-		tt <- mm[which(mm$chrom==chr),c(1:5,i), drop=F]
+		tt <- mm[which(mm$chrom==chr),c(1:5,i,grep("cf.em", colnames(mm))), drop=F]
 		tt[which(is.na(tt[,6])),6] <- -1000
 		rr <- rle(tt[,6]); 
 		if (rr$values[1]== -1000 & length(rr$values)>1) {
@@ -140,7 +140,7 @@ for (i in grep("GL", colnames(mm))) {
 		}
 		mm[which(mm$chrom==chr),i] <- as.vector(unlist(apply(cbind(rr$value,rr$length), 1, function(x){rep(x[1],x[2])})))
 
-		tt <- mm[which(mm$chrom==chr),c(1:5,i), drop=F]
+		tt <- mm[which(mm$chrom==chr),c(1:5,i,grep("cf.em", colnames(mm))), drop=F]
 		rr <- rle(tt[,6]); 
 		if (length(rr$length)>1) {
 			cs <- cumsum(rr$lengths)
@@ -155,9 +155,11 @@ for (i in grep("GL", colnames(mm))) {
 				seg_band <- c(seg_band, paste(tt[start[idx],"band"], tt[end[idx],"band"], sep="-"))
 				seg_start <- c(seg_start, tt[start[idx],"start"])
 				seg_end <- c(seg_end, tt[end[idx],"end"])
-				seg_genes <- c(seg_genes, toString(mm[start[idx]:end[idx],"hgnc"]))
+#				seg_genes <- c(seg_genes, toString(mm[start[idx]:end[idx],"hgnc"]))
+				seg_genes <- c(seg_genes, toString(tt[start[idx]:end[idx],"hgnc"]))
 				seg_type <- c(seg_type, rr$values[idx])
 				seg_GLtype <- c(seg_GLtype, colnames(mm)[i])
+				seg_cf.em <- c(seg_cf.em, tt[start[idx],grep("cf.em", colnames(tt))])
 			}
 		}		
 
@@ -168,7 +170,7 @@ for (i in grep("GL", colnames(mm))) {
 
 seg_type[which(seg_type==2)] <- "amp"
 seg_type[which(seg_type== -2)] <- "del"
-write.table(cbind(seg_sample, seg_chr, seg_band, seg_start, seg_end, seg_genes, seg_type, seg_GLtype), 
+write.table(cbind(seg_sample, seg_chr, seg_band, seg_start, seg_end, seg_genes, seg_type, seg_GLtype, seg_cf.em), 
 	file=paste(opt$outFile, ".ampdel.txt", sep=""), sep="\t", row.names=F, na="", quote=F)
 
 cat ("Done writing amp/del files\n")
