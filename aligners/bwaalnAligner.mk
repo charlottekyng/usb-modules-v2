93a8a1585174301ff3a2a4a5c9ec7cc9259e4e65
Andrej Benjak <abenjak@gmail.com>
2023-05-09 13:17:21 +0200

    :bug: sort bam after bwaaln

--- aligners/bwaalnAligner.mk
+++ aligners/bwaalnAligner.mk
@@ -35,19 +35,19 @@ ifeq ($(MERGE_SPLIT_FASTQ),true)
 bwaaln/bam/%.bwaaln.bam : bwaaln/sai/%.1.sai $(if $(findstring true,$(PAIRED_END)),bwaaln/sai/%.2.sai) \
 fastq/%.1.fastq.gz $(if $(findstring true,$(PAIRED_END)),fastq/%.2.fastq.gz)
 	LBID=`echo "$*" | sed 's/_[A-Za-z0-9]\+//'`; \
-	$(call RUN,$(BWA_NUM_CORES),$(RESOURCE_REQ_LOW_MEM),$(RESOURCE_REQ_MEDIUM),$(BWA_MODULE) $(SAMTOOLS_MODULE),"\
+	$(call RUN,$(BWA_NUM_CORES),$(RESOURCE_REQ_HIGH_MEM),$(RESOURCE_REQ_MEDIUM),$(BWA_MODULE) $(SAMTOOLS_MODULE),"\
 	$(if $(findstring true,$(PAIRED_END)),$(BWA_SAMPE) -P,$(BWA_SAMSE)) \
 	-r \"@RG\tID:$*\tLB:$${LBID}\tPL:${SEQ_PLATFORM}\tSM:$${LBID}\" $(REF_FASTA) $^ | \
-	$(SAMTOOLS) view -bhS - > $@")
+	$(SAMTOOLS) view -uhS - | $(SAMTOOLS) sort -T /scratch/$@.tmp -m 20G -o $@")
 else
 define align-split-fastq
 bwaaln/bam/$1.bwaaln.bam : bwaaln/sai/$1.1.sai $(if $(findstring true,$(PAIRED_END)),bwaaln/sai/$1.2.sai) \
 fastq/$1.1.fastq.gz $$(if $$(findstring true,$$(PAIRED_END)),fastq/$1.2.fastq.gz)
 	LBID=`echo "$1" | sed 's/_[A-Za-z0-9]\+//'`; \
-	$$(call RUN,$$(BWA_NUM_CORES),$$(RESOURCE_REQ_LOW_MEM),$$(RESOURCE_REQ_MEDIUM),$$(BWA_MODULE) $$(SAMTOOLS_MODULE),"\
+	$$(call RUN,$$(BWA_NUM_CORES),$$(RESOURCE_REQ_HIGH_MEM),$$(RESOURCE_REQ_MEDIUM),$$(BWA_MODULE) $$(SAMTOOLS_MODULE),"\
 	$$(if $$(findstring true,$$(PAIRED_END)),$$(BWA_SAMPE) -P,$$(BWA_SAMSE)) \
 	-r \"@RG\tID:$1\tLB:$$$${LBID}\tPL:$${SEQ_PLATFORM}\tSM:$$$${LBID}\" $$(REF_FASTA) $$^ | \
-	$$(SAMTOOLS) view -bhS - > $$@")
+	$$(SAMTOOLS) view -uhS - | $$(SAMTOOLS) sort -T /scratch/$$@.tmp -m 20G -o $$@")
 endef
 $(foreach sample,$(SPLIT_SAMPLES),$(foreach split,$(split.$(sample)),$(eval $(call align-split-fastq,$(split)))))
 
