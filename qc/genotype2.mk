4c58ffa95c04f5d07388ddb85fc44ddc513c8104
Andrej Benjak <abenjak@gmail.com>
2024-02-20 15:05:06 +0100

    :wrench: increase time for genotype2

--- qc/genotype2.mk
+++ qc/genotype2.mk
@@ -16,7 +16,7 @@ endif
 
 ifneq ($(findstring RNA,$(CAPTURE_METHOD)),RNA)
 genotype/BAMixChecker/Total_result.txt : $(foreach sample,$(SAMPLES),bam/$(sample).bam)
-	$(call RUN,6,$(RESOURCE_REQ_VHIGH_MEM),$(RESOURCE_REQ_VSHORT),$(SINGULARITY_MODULE),"\
+	$(call RUN,6,$(RESOURCE_REQ_VHIGH_MEM),$(RESOURCE_REQ_SHORT),$(SINGULARITY_MODULE),"\
 	$(SINGULARITY_EXEC) $(BAMIXCHECKER_IMG) python /BAMixChecker-1.0.1/BAMixChecker.py -d bam \
 	$(if $(findstring BAITS,$(CAPTURE_METHOD)),-b $(TARGETS_FILE_INTERVALS),if $(findstring PCR,$(CAPTURE_METHOD)),-b $(TARGETS_FILE_INTERVALS),) \
 	-r $(REF_FASTA) \
@@ -31,7 +31,7 @@ bam_splitncigar/%.splitncigar.bam : bam/%.bam
 	-R $(REF_FASTA) -I $< -O $@")
 
 genotype/BAMixChecker/Total_result.txt : $(foreach sample,$(SAMPLES),bam_splitncigar/$(sample).splitncigar.bam)
-	$(call RUN,6,$(RESOURCE_REQ_VHIGH_MEM),$(RESOURCE_REQ_VSHORT),$(SINGULARITY_MODULE),"\
+	$(call RUN,6,$(RESOURCE_REQ_VHIGH_MEM),$(RESOURCE_REQ_SHORT),$(SINGULARITY_MODULE),"\
 	$(SINGULARITY_EXEC) $(BAMIXCHECKER_IMG) python /BAMixChecker-1.0.1/BAMixChecker.py -d bam_splitncigar \
 	-r $(REF_FASTA) \
 	-o genotype --OFFFileNameMatching -p 6")
