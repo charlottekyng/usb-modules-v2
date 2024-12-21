ec0ea258306960ee13b0654e991098184969cccf
charlottekyng <charlottekyng@gmail.com>
2024-12-21 17:40:33 +0100

    strelka2 needs singularity module loaded

--- variant_callers/somatic/strelka2_somatic.mk
+++ variant_callers/somatic/strelka2_somatic.mk
@@ -25,14 +25,14 @@ strelka2_tables : $(foreach type,$(VARIANT_TYPES),$(call MAKE_TABLE_FILE_LIST,$(
 define strelka2-tumor-normal
 # If USE_BAM_CLIPOVERLAP then look for BAMs in bam_clipoverlap folder.
 strelka2/$1_$2/runWorkflow.py : $(if $(findstring true,$(USE_BAM_CLIPOVERLAP)),bam_clipoverlap,bam)/$1.bam $(if $(findstring true,$(USE_BAM_CLIPOVERLAP)),bam_clipoverlap,bam)/$2.bam
-	$$(call RUN,1,$$(RESOURCE_REQ_LOW_MEM),$$(RESOURCE_REQ_VSHORT),$$(PERL_MODULE),"\
+	$$(call RUN,1,$$(RESOURCE_REQ_LOW_MEM),$$(RESOURCE_REQ_VSHORT),$$(PERL_MODULE) $$(SINGULARITY_MODULE),"\
 	rm -rf $$(@D) && \
 	$$(CONFIGURE_STRELKA2_SOMATIC) $$(if $$(findstring NONE,$$(PANEL)),,--exome) --tumorBam $$< --normalBam $$(<<) \
 	--referenceFasta $$(REF_FASTA) --config $$(STRELKA2_SOMATIC_CONFIG) --runDir $$(@D)")
 
 # Because we use 8 cores, mem per cpu request becomes unreasonably high (and job won't run if RESOURCE_REQ_LOW_MEM is set to a very high value). Use a fixed mem value instead (strelka2 uses very little RAM).
 strelka2/$1_$2/results/variants/somatic.indels.vcf.gz : strelka2/$1_$2/runWorkflow.py
-	$$(call RUN,$$(STRELKA2_NUM_CORES),2GB,$$(RESOURCE_REQ_SHORT),,"\
+	$$(call RUN,$$(STRELKA2_NUM_CORES),2GB,$$(RESOURCE_REQ_SHORT),$$(SINGULARITY_MODULE),"\
 	$$(STRELKA2) $$(<D)/runWorkflow.py -j $$(STRELKA2_NUM_CORES) -m local")
 
 strelka2/$1_$2/results/variants/somatic.snvs.vcf.gz : strelka2/$1_$2/results/variants/somatic.indels.vcf.gz
