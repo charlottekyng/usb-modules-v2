d7d59145d5d0fb7f3c69cb4d6fa5cc92060b845a
charlottekyng <charlottekyng@gmail.com>
2025-01-31 14:57:16 +0100

    Muse: updated memory requirements

--- variant_callers/somatic/muse.mk
+++ variant_callers/somatic/muse.mk
@@ -22,20 +22,12 @@ muse_tables : $(call MAKE_TABLE_FILE_LIST,muse)
 
 define muse-tumor-normal
 muse/$1_$2.MuSE.txt : bam/$1.bam bam/$2.bam
-	$$(call RUN,$$(MUSE_NUM_CORES),$$(RESOURCE_REQ_VVHIGH_MEM),$$(RESOURCE_REQ_LONG),$$(SINGULARITY_MODULE),"\
-	$$(MUSE) call \
-	-f $$(REF_FASTA) \
-	-O muse/$1_$2 \
-	-n $$(MUSE_NUM_CORES) \
-	$$^")
+	$$(call RUN,$$(MUSE_NUM_CORES),$$(RESOURCE_REQ_VHIGH_MEM),$$(RESOURCE_REQ_LONG),$$(SINGULARITY_MODULE),"\
+	$$(MUSE) call -f $$(REF_FASTA) -O muse/$1_$2 -n $$(MUSE_NUM_CORES) $$^")
 
 muse/$1_$2.MuSE.vcf : muse/$1_$2.MuSE.txt
-	$$(call RUN,$$(MUSE_NUM_CORES),$$(RESOURCE_REQ_LOW_MEM),$$(RESOURCE_REQ_SHORT),$$(SINGULARITY_MODULE),"\
-	$$(MUSE) sump \
-	-I $$^ \
-	-O $$@ \
-	-n $$(MUSE_NUM_CORES) \
-	-D $$(DBSNP_COMMON) \
+	$$(call RUN,$$(MUSE_NUM_CORES),$$(RESOURCE_REQ_VHIGH_MEM),$$(RESOURCE_REQ_SHORT),$$(SINGULARITY_MODULE),"\
+	$$(MUSE) sump -I $$^ -O $$@ -n $$(MUSE_NUM_CORES) -D $$(DBSNP_COMMON) \
 	$$(if $$(findstring NONE,$$(BAITS)),-G,-E)")
 
 vcf/$1_$2.muse.vcf : muse/$1_$2.MuSE.vcf
