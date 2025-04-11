558dc96125cf0c4313594845b10eb91344f23434
fmarchiano <fabio.marchiano.phd@gmail.com>
2025-04-11 18:13:49 +0200

    added all the ref input files needed for brass

--- sv_callers/brass.mk
+++ sv_callers/brass.mk
@@ -10,23 +10,30 @@ PHONY += brass
 .SECONDARY:
 .PHONY: $(PHONY)
 
-HIGH_DEPTH_BED = /mnt/longterm/ng_piscuoglio/data/pnrr_pdac_wgs/brass_files/merged/depth_mask.bed
+HIGH_DEPTH_BED = brass_files/merged/depth_mask.bed
+VIRAL = $(IMG_DIR)/BRASS/ref/viral/2bit/viral.genomic.fa.2bit
+MICROBIAL = $(IMG_DIR)/BRASS/ref/bacterial/2bit
+CENTTEL = $(IMG_DIR)/BRASS/ref/centtel.bed
+GCBINS = $(IMG_DIR)/BRASS/ref/gcBins.bed.gz.tbi
+CYTOBAND = $(IMG_DIR)/BRASS/ref/cytoband.bed
 GENOME_CACHE = $(GROUP_DIR)/ref/genomes/hg38/cache/vagrent.human.GRCh38.homo_sapiens_core_80_38.cache.gz
 BRASS_PROTOCOL = WGS # WGS|WXS|RNA
-BRASS_OPTS = -g $(REF_FASTA) -s HUMAN -as 38 -pr $(BRASS_PROTOCOL) -gc $(GENOME_CACHE) -d $(HIGH_DEPTH_BED)
+SAMPSTAT = brass_files/sampstat
+
+BRASS_OPTS = -g $(REF_FASTA) -s HUMAN -as 38 -pr $(BRASS_PROTOCOL) -gc $(GENOME_CACHE) -d $(HIGH_DEPTH_BED) -vi $(VIRAL) -mi $(MICROBIAL) -ct $(CENTTEL) -b $(GCBINS) -cb $(CYTOBAND)
 
 brass : $(foreach pair,$(SAMPLE_PAIRS),brass/$(pair).brass_timestamp)
 
 bam/%.bam.bas : bam/%.bam
-	$$(call RUN,1,6G,$$(RESOURCE_REQ_MEDIUM),$$(SINGULARITY_MODULE),"\
-	$$(BRASS) bam_stats \
+	$(call RUN,1,6G,$(RESOURCE_REQ_MEDIUM),$(SINGULARITY_MODULE),"\
+	$(BRASS) bam_stats \
 	-i $< \
 	-o $@ \
 	-r $(REF_FASTA).fai")
 
 brass/genome_window.bed :
-	$$(call RUN,1,2G,$$(RESOURCE_REQ_MEDIUM),$$(SINGULARITY_MODULE),"\
-	$$(BRASS) bedtools makewindows \
+	$(call RUN,1,2G,$(RESOURCE_REQ_MEDIUM),$(SINGULARITY_MODULE),"\
+	$(BRASS) bedtools makewindows \
 	-g $(REF_FASTA) \
 	-w 10000 > $@")
 
@@ -35,6 +42,7 @@ define brass-tumor-normal
 brass/$1_$2.brass_input_timestamp : bam/$1.bam bam/$2.bam bam/$1.bam.bas bam/$2.bam.bas
 	$$(call RUN,2,6G,$$(RESOURCE_REQ_MEDIUM),$$(SINGULARITY_MODULE),"\
 	$$(BRASS) brass.pl $$(BRASS_OPTS) \
+	-ss $$(SAMPSTAT)/$2.txt \
 	-t $$< \
 	-n $$(<<) \
 	-tn $1 \
@@ -50,6 +58,7 @@ brass/$1_$2.brass_group_timestamp : bam/$1.bam bam/$2.bam bam/$1.bam.bas bam/$2.
 	-n $$(<<) \
 	-tn $1 \
 	-nn $2 \
+	-ss $$(SAMPSTAT)/$2.txt \
 	-p group \
 	-o brass/$1_$2 && touch $$@")
 
@@ -60,6 +69,7 @@ brass/$1_$2.brass_filter_timestamp : bam/$1.bam bam/$2.bam bam/$1.bam.bas bam/$2
 	-n $$(<<) \
 	-tn $1 \
 	-nn $2 \
+	-ss $$(SAMPSTAT)/$2.txt \
 	-p group \
 	-o brass/$1_$2 && touch $$@")
 
@@ -70,6 +80,7 @@ brass/$1_$2.brass_assemble_timestamp : bam/$1.bam bam/$2.bam bam/$1.bam.bas bam/
 	-n $$(<<) \
 	-tn $1 \
 	-nn $2 \
+	-ss $$(SAMPSTAT)/$2.txt \
 	-c 8 \
 	-p assemble \
 	-o brass/$1_$2 && touch $$@")
@@ -81,6 +92,7 @@ brass/$1_$2.brass_grass_timestamp : bam/$1.bam bam/$2.bam bam/$1.bam.bas bam/$2.
 	-n $$(<<) \
 	-tn $1 \
 	-nn $2 \
+	-ss $$(SAMPSTAT)/$2.txt \
 	-p grass \
 	-o brass/$1_$2 && touch $$@")
 
@@ -92,6 +104,7 @@ brass/$1_$2.brass_tabix_timestamp : bam/$1.bam bam/$2.bam bam/$1.bam.bas bam/$2.
 	-tn $1 \
 	-nn $2 \
 	-p tabix \
+	-ss $$(SAMPSTAT)/$2.txt \
 	-o brass/$1_$2 && touch $$@")
 
 brass/$1_$2.brass_timestamp : brass/$1_$2.brass_tabix_timestamp
