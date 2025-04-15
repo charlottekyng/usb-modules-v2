395dcc7f3b526a52173323409a51ff3a9f5bacba
fmarchiano <fabio.marchiano.phd@gmail.com>
2025-04-15 17:55:29 +0200

    brass fully working

--- sv_callers/brass.mk
+++ sv_callers/brass.mk
@@ -12,11 +12,11 @@ PHONY += brass
 
 HIGH_DEPTH_BED = brass_files/merged/depth_mask.bed
 VIRAL = $(IMG_DIR)/BRASS/ref/viral/2bit/viral.genomic.fa.2bit
-MICROBIAL = $(IMG_DIR)/BRASS/ref/bacterial/2bit
-CENTTEL = $(IMG_DIR)/BRASS/ref/centtel.bed
-GCBINS = $(IMG_DIR)/BRASS/ref/gcBins.bed.gz.tbi
+MICROBIAL = $(IMG_DIR)/BRASS/ref/bacterial/2bit/all_ncbi_bacteria 
+CENTTEL = $(IMG_DIR)/BRASS/ref/centtel.tsv
+GCBINS = $(IMG_DIR)/BRASS/ref/gcBins.bed.gz
 CYTOBAND = $(IMG_DIR)/BRASS/ref/cytoband.bed
-GENOME_CACHE = $(GROUP_DIR)/ref/genomes/hg38/cache/vagrent.human.GRCh38.homo_sapiens_core_80_38.cache.gz
+GENOME_CACHE = $(GROUP_DIR)/ref/genomes/hg38/cache/vagrent.human.hg38.homo_sapiens_core_80_38.cache.gz
 BRASS_PROTOCOL = WGS # WGS|WXS|RNA
 SAMPSTAT = brass_files/sampstat
 
