43efd1bc93721e159983e4aa2e00bb8111ae1ae3
fmarchiano <fabio.marchiano.phd@gmail.com>
2025-06-19 16:57:29 +0200

    add skipsoftcliprealignment for speed-up :fire:

--- sv_callers/sv_pon.mk
+++ sv_callers/sv_pon.mk
@@ -25,10 +25,10 @@ gridss/pondir/%.normal.vcf : bam/%.bam $$(REF_FASTA).gridsscache $$(REF_FASTA).d
 	$$(if $$(findstring hg38,$$(REF)),-b $$(BED_DIR)/ENCFF356LFX.bed) \
 	$$(if $$(findstring b37,$$(REF)),-b $$(BED_DIR)/ENCFF001TDO.bed) \
 	-r $$(REF_FASTA) \
-	-s preprocess,assemble,call \
+	-s preprocess$$(,)assemble$$(,)call \
 	--skipsoftcliprealignment \
 	-o $$@ \
-	$$< ")
+	$$<")
 endef
 $(foreach normal,$(PANEL_OF_NORMAL_SAMPLES),$(eval $(call gridss-pon,$(normal))))
 
