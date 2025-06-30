c87d997e006a9dc604d0c1fcf94fa02f0ce98cad
fmarchiano <fabio.marchiano.phd@gmail.com>
2025-06-30 17:56:53 +0200

    plots font ylabels fixed

--- copy_number/facetsGeneCNPlot.R
+++ copy_number/facetsGeneCNPlot.R
@@ -35,7 +35,7 @@ if (length(arguments$args) != 2) {
 # barcodes on y axis
 if ("showtext" %in% rownames(installed.packages())) {
     suppressMessages(library(showtext))
-    font_add("DejaVuSansMono", "DejaVuSansMono.ttf")
+    font_add("DejaVuSansMono", "usb-modules-v2/fonts/DejaVuSansMono.ttf")
     showtext_auto()
     fontfamily <- "DejaVuSansMono"
 } else {
@@ -44,30 +44,29 @@ if ("showtext" %in% rownames(installed.packages())) {
 
 plot_heatmap <- function(facets_tab, plot_file, sample_names=NULL, col=c("red", "darksalmon", NA, "lightblue", "blue"), zlim=c(-2,2)) {
     mm <- facets_tab
+	mm <- mm[mm$chrom %in% c(as.character(1:22), "X", "Y", "M"), ]
     if (is.null(sample_names)) { sample_names <- list(colnames(mm)[-c(1:5)]) }
     chrsep <- cumsum(rle(mm$chrom)$lengths)
     chrmid <- c(0,chrsep[-length(chrsep)]) + (rle(mm$chrom)$lengths/2)
-    
-    pdf(plot_file, width=7, height=3*length(sample_names))
+
+    pdf(plot_file, width=7, height=10*length(sample_names))
     par(mfrow=c(length(sample_names),1), mar=c(8,.5*(max(sapply(sample_names,nchar))),1,2))
     lapply(sample_names, function(x, mm) {
         mm2 <- mm[,rev(x)]; #for (i in 1:ncol(mm2)) { mm2[,i] <- as.numeric(mm2[,i]) }
         image(as.matrix(mm2), col=col, xaxt='n', yaxt='n', zlim=zlim)
         box()
         for (i in (chrsep*2)-1) { abline(v=i/((max(chrsep)-1)*2), col="grey") }
-        for (i in seq(-1, max(((2*(ncol(mm2)-1))+1),1), 2)) { abline(h=i/(2*(ncol(mm2)-1)), col="white", lwd=2)}
+        for (i in seq(-1, max(((2*(ncol(mm2)-1))+1),1), 2)) { abline(h=i/(2*(ncol(mm2)-1)), col="white", lwd=4)}
+	
+	# need to be fixed, chromosome 22 label is cut out
+	axis(1,at=(chrmid/(max(chrsep)-1))[seq(1,length(chrmid),by=2)], label=rle(mm$chrom)$values[seq(1,length(chrmid),by=2)], cex.axis=0.8, tick=F, line=-0.8, cex.axis=0.6)
+	axis(1,at=(chrmid/(max(chrsep)-1))[seq(2,length(chrmid),by=2)],label=rle(mm$chrom)$values[seq(2,length(chrmid),by=2)], cex.axis=0.8, tick=F, line=0, cex.axis=0.6)
 
-	axis(1,at=(chrmid/(max(chrsep)-1))[seq(1,length(chrmid),by=2)], 
-		label=rle(mm$chrom)$values[seq(1,length(chrmid),by=2)], cex.axis=0.8, tick=F, line=-0.8, cex.axis=0.6)
-	axis(1,at=(chrmid/(max(chrsep)-1))[seq(2,length(chrmid),by=2)], 
-		label=rle(mm$chrom)$values[seq(2,length(chrmid),by=2)], cex.axis=0.8, tick=F, line=0, cex.axis=0.6)
-
-
-#    axis(1,at=chrmid/(max(chrsep)-1), label=rle(mm$chrom)$values, cex.axis=0.8, tick=F, family=fontfamily)
-    axis(2,at=seq(0,1,1/max((ncol(mm2)-1),1)), label=unlist(lapply(colnames(mm2), function(x){strsplit(x,split="_")[[1]][1]})), las=2, cex.axis=1, tick=F)
+    #axis(1,at=chrmid/(max(chrsep)-1), label=rle(mm$chrom)$values, cex.axis=0.6, tick=F)
+    axis(2,at=seq(0,1,1/max((ncol(mm2)-1),1)), label=unlist(lapply(colnames(mm2), function(x){strsplit(x,split="_")[[1]][1]})), las=2, cex.axis=.5, tick=F)
     }, mm)
-    legend("bottom", inset=c(0,-1.2), legend=c("Homozygous deletion", "Loss", "Gain", "Amplification"),
-           fill=col[c(1,2,4,5)], xpd=T, ncol=2, bty='n')
+    legend(x=-0.1, y=-0.1, legend=c("Homozygous deletion", "Loss", "Gain", "Amplification"),
+           fill=col[c(1,2,4,5)], xpd=NA, ncol=2, bty='n')
     dev.off()
 }
 
