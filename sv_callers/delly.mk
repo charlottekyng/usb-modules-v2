2480acc851c81fd285b8d96d9b0f52d02b985ff7
charlottekyng <charlottekyng@gmail.com>
2026-02-13 13:50:31 +0100

    delly: clean up, fix quotes, use T_N file names

--- sv_callers/delly.mk
+++ sv_callers/delly.mk
@@ -3,7 +3,6 @@ MUT_CALLER = delly
 # Run delly on tumour-normal matched pairs
 
 include usb-modules-v2/Makefile.inc
-include usb-modules-v2/vcf_tools/vcftools.mk
 
 LOGDIR ?= log/delly.$(NOW)
 PHONY += delly
@@ -16,13 +15,13 @@ delly : delly_vcfs
 
 delly/sample_info.txt : $(SAMPLE_SET_FILE)
 	$(call RUN,1,$(RESOURCE_REQ_LOW_MEM),$(RESOURCE_REQ_VSHORT),,"\
-	grep -v '#' $(<) | sed -E -e "s/\t/\ttumor/g" -e "s/\$$/\tcontrol/" | sed 's/tumor/&\n/g' > $@")
+	grep -v '#' $(<) | sed -E -e 's/\t/\ttumor/g' -e 's/\$$/\tcontrol/' | sed 's/tumor/&\n/g' > $@")
 
-delly_vcfs : delly/sample_info.txt $(foreach pair,$(SAMPLE_PAIRS),delly/$(tumor.$(pair)).somatic.SVpass.vcf)
+delly_vcfs : delly/sample_info.txt $(foreach pair,$(SAMPLE_PAIRS),delly/$(tumor.$(pair))_$(normal.$(pair)).somatic.SVpass.vcf)
 
 define delly-tumor-normal
 # Main call
-delly/$1.bcf : bam/$1.bam bam/$2.bam
+delly/$1_$2.bcf : bam/$1.bam bam/$2.bam
 	$$(MKDIR) $$(@D)
 	$$(call RUN,1,$$(RESOURCE_REQ_HIGH_MEM),$$(RESOURCE_REQ_MEDIUM),$$(SINGULARITY_MODULE),"\
 	$$(DELLY) delly call \
@@ -34,7 +33,7 @@ delly/$1.bcf : bam/$1.bam bam/$2.bam
 	$$^")
 
 # Somatic pre-filtering
-delly/$1.pre.bcf : delly/$1.bcf delly/sample_info.txt
+delly/$1_$2.pre.bcf : delly/$1_$2.bcf delly/sample_info.txt
 	$$(call RUN,1,1G,$$(RESOURCE_REQ_MEDIUM),$$(SINGULARITY_MODULE),"\
 	$$(DELLY) delly filter \
 	-f somatic \
@@ -45,7 +44,7 @@ delly/$1.pre.bcf : delly/$1.bcf delly/sample_info.txt
 # Genotype pre-filtered somatic sites across all normal samples.
 # Do it with all normals in one go. If this becomes too slow with many normals modify this part to run per each normal and then merge.
 # Eventually we might want to be able to pass external normals as well.
-delly/$1.geno.bcf : delly/$1.pre.bcf bam/$1.bam bam/$2.bam $(foreach normal,$(PANEL_OF_NORMAL_SAMPLES),bam/$(normal).bam)
+delly/$1_$2.geno.bcf : delly/$1_$2.pre.bcf bam/$1.bam bam/$2.bam $(foreach normal,$(PANEL_OF_NORMAL_SAMPLES),bam/$(normal).bam)
 	$$(call RUN,1,$$(RESOURCE_REQ_HIGH_MEM),$$(RESOURCE_REQ_LONG),$$(SINGULARITY_MODULE),"\
 	$$(DELLY) delly call \
 	$$(if $$(findstring hg38,$$(REF)),-x $$(BED_DIR)/human.hg38.excl.bed) \
@@ -57,7 +56,7 @@ delly/$1.geno.bcf : delly/$1.pre.bcf bam/$1.bam bam/$2.bam $(foreach normal,$(PA
 	$$(filter-out $$<,$$^)")
 
 # Post-filter
-delly/$1.somatic.bcf : delly/$1.geno.bcf delly/sample_info.txt
+delly/$1_$2.somatic.bcf : delly/$1_$2.geno.bcf delly/sample_info.txt
 	$$(call RUN,1,1G,$$(RESOURCE_REQ_VSHORT),$$(SINGULARITY_MODULE),"\
 	$$(DELLY) delly filter \
 	-f somatic \
@@ -65,13 +64,11 @@ delly/$1.somatic.bcf : delly/$1.geno.bcf delly/sample_info.txt
 	-s $$(<<) \
 	$$<")
 
-# convert to vcf
-delly/$1.somatic.vcf : delly/$1.somatic.bcf
-	$$(call RUN,1,1G,$$(RESOURCE_REQ_VSHORT),$$(BCFTOOLS_MODULE),"\
-	bcftools view $$^ > $$@")
-
-# filter to vcf PASS only
-delly/$1.somatic.SVpass.vcf : delly/$1.somatic.vcf
+delly/$1_$2.somatic.vcf : delly/$1_$2.somatic.bcf
+	$$(call RUN,1,$$(RESOURCE_REQ_LOW_MEM),$$(RESOURCE_REQ_VSHORT),$$(BCFTOOLS_MODULE),"\
+	$$(BCFTOOLS) view $$< > $$@")
 
 endef
 $(foreach pair,$(SAMPLE_PAIRS),$(eval $(call delly-tumor-normal,$(tumor.$(pair)),$(normal.$(pair)))))
+
+include usb-modules-v2/vcf_tools/vcftools.mk
