039fb94d20a1df9acfe21a33458b02a456d03302
charlottekyng <charlottekyng@gmail.com>
2026-02-16 09:37:24 +0100

    brass: fixed missing escapes, targets should be last file generated, use T_N notation

--- sv_callers/brass.mk
+++ sv_callers/brass.mk
@@ -4,7 +4,6 @@
 MUT_CALLER = brass
 
 include usb-modules-v2/Makefile.inc
-include usb-modules-v2/vcf_tools/vcftools.mk
 
 LOGDIR = log/brass.$(NOW)
 PHONY += brass
@@ -24,48 +23,46 @@ BRASS_PROTOCOL = WGS# WGS|WXS|RNA
 
 BRASS_OPTS = -g $(REF_FASTA) -s HUMAN -as 38 -pr $(BRASS_PROTOCOL) -gc $(GENOME_CACHE) -d $(HIGH_DEPTH_BED) -vi $(VIRAL) -mi $(MICROBIAL) -ct $(CENTTEL) -b $(GCBINS) -cb $(CYTOBAND)
 
-brass : brass_files/merged/depth_mask.bed $(foreach pair,$(SAMPLE_PAIRS),brass/$(pair).brass_wrap.log)
+brass : $(foreach pair,$(SAMPLE_PAIRS),brass/$(pair)/$(tumor.$(pair))_vs_$(normal.$(pair)).intermediates.tar.gz)
+#brass : $(foreach pair,$(SAMPLE_PAIRS),brass/$(pair).brass_wrap.log)
 
 bam/%.bam.bas : bam/%.bam
 	$(call RUN,1,6G,$(RESOURCE_REQ_MEDIUM),$(SINGULARITY_MODULE),"\
-	$(BRASS) bam_stats \
-	-i $< \
-	-o $@ \
-	-r $(REF_FASTA).fai")
+	$(BRASS) bam_stats -i $< -o $@ -r $(REF_FASTA).fai")
 
 brass_files/%.bw : bam/%.bam
 	$(MKDIR) $(@D)
-	$(call RUN,1,6G,$(RESOURCE_REQ_MEDIUM),$(SINGULARITY_MODULE),"\
-	$(CPGBIGWIG) bam2bw \
-	-i $< \
-	-o $@")
+	$(call RUN,1,12G,$(RESOURCE_REQ_MEDIUM),$(SINGULARITY_MODULE),"\
+	$(CPGBIGWIG) bam2bw -i $< -o $@")
 
 brass_files/depth/%.bed : brass_files/%.bw
 	$(MKDIR) $(@D)
 	$(call RUN,1,6G,$(RESOURCE_REQ_MEDIUM),$(SINGULARITY_MODULE),"\
-	$(CPGBIGWIG) detectExtremeDepth \
-	-b $< \
-	-o brass_files/depth/ ")
+	$(CPGBIGWIG) detectExtremeDepth -b $< -o brass_files/depth/ ")
 
 brass_files/sorted/%.bed: brass_files/depth/%.bed
 	$(MKDIR) $(@D)
 	$(call RUN,1,$(RESOURCE_REQ_LOW_MEM),$(RESOURCE_REQ_VSHORT),,"\
-	cat $< | sort -k1,1 -k2,2n > $@")
+	cat $< | sort -k1$(,)1 -k2$(,)2n > $@")
 
+ifneq ($(words $(foreach pair,$(SAMPLE_PAIRS),brass_files/sorted/$(tumor.$(pair)).bed)),1)
 brass_files/merged/intersect.bed: $(foreach pair,$(SAMPLE_PAIRS),brass_files/sorted/$(tumor.$(pair)).bed)
 	$(call RUN,1,$(RESOURCE_REQ_LOW_MEM),$(RESOURCE_REQ_VSHORT),$(BEDTOOLS_MODULE),"\
 	$(BEDTOOLS) multiinter -i $^ > $@")
+else
+brass_files/merged/intersect.bed : $(foreach pair,$(SAMPLE_PAIRS),brass_files/sorted/$(tumor.$(pair)).bed)
+	cp $< $@ && sleep 3;
+endif
 
 brass_files/merged/intersect.filtered.bed: brass_files/merged/intersect.bed
 	$(call RUN,1,$(RESOURCE_REQ_LOW_MEM),$(RESOURCE_REQ_VSHORT),$(PERL_MODULE),"\
-	perl -ane 'next if ($$F[3] < 3); print $$F[0]$(comma)qq{\t}$(comma)$$F[1]$(comma)qq{\t}$(comma)$$F[2]$(comma)qq{\n};' $< \
-	| sort -k1$(comma)1 -k2$(comma)2n \
-	> $@")
+	perl -ane 'next if (\$$F[3] < 3); print \$$F[0]$(comma)qq{\t}$(comma)\$$F[1]$(comma)qq{\t}$(comma)\$$F[2]$(comma)qq{\n};' $< \
+	| sort -k1$(comma)1 -k2$(comma)2n > $@")
 
 brass_files/merged/depth_mask.bed.gz: brass_files/merged/intersect.filtered.bed
 	$(call RUN,1,$(RESOURCE_REQ_LOW_MEM),$(RESOURCE_REQ_VSHORT),$(BEDTOOLS_MODULE),"\
 	$(BEDTOOLS) merge -i $< -d 250 \
-	| perl -ane 'next if ($$F[2]-$$F[1] < 500); print;' \
+	| perl -ane 'next if (\$$F[2]-\$$F[1] < 500); print;' \
 	| $(BGZIP) -c > $@")
 
 brass_files/merged/depth_mask.bed.gz.tbi: brass_files/merged/depth_mask.bed.gz
@@ -76,27 +73,22 @@ brass_files/merged/depth_mask.bed: brass_files/merged/depth_mask.bed.gz
 	$(call RUN,1,$(RESOURCE_REQ_LOW_MEM),$(RESOURCE_REQ_VSHORT),,"\
 	$(GUNZIP) $< > $@")
 
-
-brass_files/sampstat/%.txt: facets/cncf/all.summary.txt
+brass_files/sampstat/%.txt : facets/cncf/all.summary.txt
 	$(MKDIR) $(@D)
 	$(call RUN,1,$(RESOURCE_REQ_LOW_MEM),$(RESOURCE_REQ_VSHORT),,"\
-	awk -v sample="$*" 'BEGIN{FS="\t"} NR > 1 && $$3 == sample { \
-		if ($$9 != "NA" && $$10 != "NA") { \
-			printf("rho\t%s\nPloidy\t%s\nGenderChr\tY\nGenderChrFound\tY\n", $$9, $$10); \
+	awk -v sample=\"$*\" 'BEGIN{FS=\"\t\"} NR > 1 && \$$3 == sample { \
+		if (\$$9 != \"NA\" && \$$10 != \"NA\") { \
+		printf(\"rho\t%s\nPloidy\t%s\nGenderChr\tY\nGenderChrFound\tY\n\", \$$9, \$$10); \
 		} \
 	}' $< > $@")
 
 define brass-tumor-normal
-brass/$1_$2.brass_wrap.log : bam/$1.bam bam/$2.bam bam/$1.bam.bas bam/$2.bam.bas brass_files/merged/depth_mask.bed brass_files/sampstat/$1.txt
-	$$(call RUN,2,32G,$$(RESOURCE_REQ_MEDIUM),$$(SINGULARITY_MODULE),"\
+brass/$1_$2/$1_vs_$2.intermediates.tar.gz : bam/$1.bam bam/$2.bam bam/$1.bam.bas bam/$2.bam.bas brass_files/merged/depth_mask.bed brass_files/sampstat/$1.txt
+	$$(call RUN,2,64G,$$(RESOURCE_REQ_MEDIUM),$$(SINGULARITY_MODULE),"\
 	$$(BRASS) brass.pl $$(BRASS_OPTS) \
-	-ss $$(<<<<<<) \
-	-t $$< \
-	-n $$(<<) \
-	-tn $1 \
-	-nn $2 \
-	-c 2 \
-	-o brass/$1_$2 && touch $$@")
+	-ss $$(<<<<<<) -t $$< -n $$(<<) -tn $1 -nn $2 -c 2 -o brass/$1_$2 && touch $$@")
 
 endef
 $(foreach pair,$(SAMPLE_PAIRS),$(eval $(call brass-tumor-normal,$(tumor.$(pair)),$(normal.$(pair)))))
+
+include usb-modules-v2/vcf_tools/vcftools.mk
